2a6570f4904dcafc73896e84850f3ba3622ea799
caronnee <caronnee@01adf37a-b619-f3a3-acd6-66f42c73fe0f>
2012-10-03 22:33:46 +0000

    one fractal showed

--- circle.h
+++ circle.h
@@ -17,7 +17,11 @@ private:
 	float _alpha;
 	Circle * _attached;
 	float _rDiff;
-public slots:
+	float _scale;
+	QPointF _point;
+	QColor _color;
+	bool _cross;
+	public slots:
 	void attach( Circle * at );
 	virtual void advance(int phase);
 public:
@@ -26,6 +30,7 @@ public:
 	void setParent( Circle* param1 );
 	int getRange();
 	void setRange( int range );
+	void setCross(QPointF point, QColor color);
 };
 
 #endif // CIRCLE_H
